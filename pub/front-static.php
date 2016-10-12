7b02aa45a2ef658c813239dd8fda23af38e8b3b5
Dale Sikkema <dsikkema@magento.com>
2016-10-12 13:38:17 -0500

    Create template for Magento 2.0.10

--- pub/front-static.php
+++ pub/front-static.php
@@ -5,8 +5,7 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-// This is a copy of static.php because the Platform.sh Nginx configuration
-// has a bug that prevents us from using both /static.php and /static.
+
 require __DIR__ . '/../app/bootstrap.php';
 $bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
 /** @var \Magento\Framework\App\StaticResource $app */
