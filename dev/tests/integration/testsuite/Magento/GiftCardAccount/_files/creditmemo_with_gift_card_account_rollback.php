59f26647dac042e46b52813669ba1ed17f67a561
Nick Geron <ngeron@magento.com>
2017-09-01 12:10:26 -0500

    Update to 2.2.0-rc15 added dev/tests to gitignore

--- dev/tests/integration/testsuite/Magento/GiftCardAccount/_files/creditmemo_with_gift_card_account_rollback.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-require 'order_with_gift_card_account_rollback.php';
--- dev/tests/integration/testsuite/Magento/Rma/_files/rma_list_rollback.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-require __DIR__ . '/../../../Magento/Sales/_files/order_list_rollback.php';
-require __DIR__ . '/../../../Magento/Catalog/_files/product_simple_rollback.php';
