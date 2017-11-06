dedf3f2f67bed4f96f2259d33424aaeb9eb43290
Oleh Posyniak <oposyniak@magento.com>
2017-11-06 09:31:35 +0200

    MAGECLOUD-1153: Create Cloud Deliverables for Magento Release 2.0.17

--- dev/tests/static/framework/tests/unit/testsuite/Magento/TestFramework/Inspection/_files/words_finder/twilight/eclipse.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-echo 'As a string of mysterious killings grips Seattle, Bella, whose high school graduation is fast approaching,
-    is forced to choose between her love for vampire Edward and her friendship with demon Jacob.';
