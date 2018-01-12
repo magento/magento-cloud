f74629e71578aaf4af9a2774f109ade80e121779
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2018-01-13 00:21:39 +0200

    MAGECLOUD-1180: Make ECE Template Cleaner

--- dev/tests/integration/testsuite/Magento/AdminGws/_files/role_stores_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-$scope = 'stores';
-include __DIR__ . '/role_websites_login.php';
--- dev/tests/integration/testsuite/Magento/Theme/Controller/Adminhtml/System/Design/_files/simple-js-file.js
+++
@@ -1,5 +0,0 @@
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-alert('to be or not to be');
