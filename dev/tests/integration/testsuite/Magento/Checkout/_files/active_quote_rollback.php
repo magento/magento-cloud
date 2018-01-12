e026b369d5f417839f615057a8b1b12c33c65988
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2018-01-13 01:25:47 +0200

    MAGECLOUD-1180: Make ECE Template Cleaner

--- dev/tests/integration/testsuite/Magento/Checkout/_files/active_quote_rollback.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-$quote = $objectManager->create('Magento\Quote\Model\Quote');
-$quote->load('test_order_1', 'reserved_order_id')->delete();
--- dev/tests/integration/testsuite/Magento/Sales/_files/quote_with_customer_rollback.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-include __DIR__ . '/quote_rollback.php';
-include __DIR__ . '/../../../Magento/Customer/_files/customer_rollback.php';
