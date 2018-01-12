de34fae380e2ab3d99c4685f7af0319098d55d3d
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2018-01-13 01:00:18 +0200

    MAGECLOUD-1180: Make ECE Template Cleaner

--- dev/tests/js/jasmine/assets/jsbuild/external.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-define([], function () {
-    'use strict';
-
-    return 'external module';
-});
