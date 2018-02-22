fb03e82e435887fd8357f85a87e7bfce651e3fd2
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2018-02-22 16:16:00 +0200

    MAGECLOUD-1180: Make ECE Template Cleaner (#71)

--- dev/tests/js/jasmine/assets/jsbuild/external.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-define([], function () {
-    'use strict';
-
-    return 'external module';
-});
--- dev/tests/static/framework/tests/unit/testsuite/Magento/TestFramework/Inspection/_files/words_finder/interview_with_the_vampire.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-echo 'A man tells his epic life story: love, betrayal, loneliness, and hunger.';
