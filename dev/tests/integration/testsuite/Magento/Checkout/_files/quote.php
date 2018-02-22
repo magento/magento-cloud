3668771bc3e206a8621b113f8273df0f3f243e4b
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2018-02-22 16:14:01 +0200

    MAGECLOUD-1180: Make ECE Template Cleaner (#75)

--- dev/tests/integration/testsuite/Magento/Checkout/_files/quote.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-$quote = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Quote\Model\Quote');
-$quote->setData(['store_id' => 1, 'is_active' => 0, 'is_multi_shipping' => 0]);
-$quote->save();
--- dev/tests/static/framework/tests/unit/testsuite/Magento/TestFramework/Inspection/_files/words_finder/interview_with_the_vampire.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-echo 'A man tells his epic life story: love, betrayal, loneliness, and hunger.';
