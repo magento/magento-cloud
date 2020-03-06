7c06ea6209281e6201e83f982e5f8801f702de65
Oleh Posyniak <oposyniak@magento.com>
2020-03-06 10:10:25 -0600

    MAGECLOUD-5481: Update Magento Cloud template licenses for files

--- magento-vars.php
+++ magento-vars.php
@@ -1,11 +1,23 @@
 <?php
-// enable, adjust and copy this code for each store you run
-// Store #0, default one
-//if (isHttpHost("example.com")) {
-//    $_SERVER["MAGE_RUN_CODE"] = "default";
-//    $_SERVER["MAGE_RUN_TYPE"] = "store";
-//}
-function isHttpHost($host)
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Enable, adjust and copy this code for each store you run
+ *
+ * Store #0, default one
+ *
+ * if (isHttpHost("example.com")) {
+ *    $_SERVER["MAGE_RUN_CODE"] = "default";
+ *    $_SERVER["MAGE_RUN_TYPE"] = "store";
+ * }
+ *
+ * @param string $host
+ * @return bool
+ */
+function isHttpHost(string $host)
 {
     if (!isset($_SERVER['HTTP_HOST'])) {
         return false;
